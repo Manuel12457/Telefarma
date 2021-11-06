b7648437475282be8b7524f0142afb4b39cb1f2d
Percy De La Rosa <a20192265@pucp.edu.pe>
2021-11-06 12:17:54 -0500

    Ahora se pueden obtener imagenes de la DB

--- src/main/java/com/example/telefarma/daos/ImageDao.java
+++ src/main/java/com/example/telefarma/daos/ImageDao.java
@@ -1,6 +1,45 @@
 package com.example.telefarma.daos;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+import java.util.ArrayList;
+
 public class ImageDao {
 
+    public byte[] obtenerImagen(String id){
+
+        byte[] content = null;
+
+        String user = "root";
+        String psw = "root";
+        String url = "jdbc:mysql://localhost:3306/telefarma";
+
+        String sql = "SELECT photo FROM product\n"+
+                "WHERE idProduct="+id;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try (Connection conn = DriverManager.getConnection(url,user,psw);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            if (rs.next()) {
+                content = rs.getBytes("photo");
+            } else{
+                content = new byte[]{(byte) 1};
+            }
+            return content;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        content = new byte[]{(byte) 0};
+        return content;
+    }
 
 }
--- src/main/java/com/example/telefarma/servlets/ImageServlet.java
+++ src/main/java/com/example/telefarma/servlets/ImageServlet.java
@@ -1,34 +1,38 @@
 package com.example.telefarma.servlets;
 
+import com.example.telefarma.daos.ImageDao;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.*;
 
-//@WebServlet(name = "ServletPrueba", value = "/Image")
-//public class ImageServlet extends HttpServlet {
-//
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//
-//        String id = request.getParameter('id');
-//
-//        try (ResultSet resultSet = statement.executeQuery()) {
-//            if (resultSet.next()) {
-//                byte[] content = resultSet.getBytes("content");
-//                response.setContentType(getServletContext().getMimeType(imageName));
-//                response.setContentLength(content.length);
-//                response.getOutputStream().write(content);
-//            } else {
-//                response.sendError(HttpServletResponse.SC_NOT_FOUND); // 404.
-//            }
-//        }
-//    } catch (SQLException e) {
-//        throw new ServletException("Something failed at SQL/DB level.", e);
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//
-//    }
-//}
+@WebServlet(name = "ImageServlet", value = "/Image")
+public class ImageServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String id = request.getParameter("id");
+
+        ImageDao imgd = new ImageDao();
+
+        byte[] content = imgd.obtenerImagen(id);
+
+        if(content.length==1 && content[0]==0){
+            System.out.println("Algo falló al nivel de SQL/DB");
+        }else if(content.length==1 && content[0]==1){
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }else{
+            response.setContentType("image/gif");
+            response.setContentLength(content.length);
+            response.getOutputStream().write(content);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
